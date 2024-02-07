f946736a8b0a2824a40fbbeaf8461faa7527bdd4
ugurhan <apaydiugurhan@gmail.com>
2024-02-07 11:31:26 +0300

    python1deneme

---
+++ deneme1.py
@@ -0,0 +1,11 @@
+sual = "el-cevab" 
+
+if sual:True
+
+spam = 2 #bruh 
+         #bruh 2
+text = "#this is not buruh its inside quote"
+
+print(sual)
+
+
---
+++ sayilar.py
@@ -0,0 +1,18 @@
+#     arithmetic operators
+
+# addition:        3 + 2
+# subtraction:     3 - 2
+# division:        3 / 2
+# multiplication:  3 * 2
+# floor division:  3 // 2
+# exponents:       3 ** 2
+# modulus:         3 % 2
+
+# num = -3
+
+#num *= 10
+#>>> num x 10
+
+#print(abs(num))
+# abs()  mutlak değerle yazılır
+
